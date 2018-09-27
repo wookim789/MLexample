d819e959b58585a945a7e612080d80d200105a73
wookim789 <wookim789@gmail.com>
2018-09-27 13:38:27 +0900

    이력서_딥러닝프로젝트

---
+++ DeepLearningProject/Titanic.py
@@ -0,0 +1,117 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+import tensorflow as tf
+import csv
+import keras 
+import dataProcessClass as dpc
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout 
+from pandas import DataFrame, Series
+
+trainFile = pd.read_csv('C:/Users/Playdata/Desktop/Playdata/PlayData/DeepLearningProject/data/train.csv')
+testFile =  pd.read_csv('C:/Users/Playdata/Desktop/Playdata/PlayData/DeepLearningProject/data/test.csv')
+testAnswerFile =  pd.read_csv('C:/Users/Playdata/Desktop/Playdata/PlayData/DeepLearningProject/data/gender_submission.csv')
+
+trainFrame = DataFrame(trainFile)
+testFrame = DataFrame(testFile)
+testAnswerFrame = DataFrame(testAnswerFile)
+print(trainFrame.describe())
+
+trainAnswerFrame = trainFrame['Survived']
+del trainFrame['Survived']
+del trainFrame['Ticket']
+del trainFrame['Cabin']
+del trainFrame['Name']
+#test data 제거 항목 
+del testFrame['Ticket']
+del testFrame['Cabin']
+del testFrame['Name']
+#testAnswer data 제거 항목
+del testAnswerFrame["PassengerId"]
+
+print(trainFrame.ix[:,1].value_counts())
+
+
+
+dp = dpc.dataProcess(trainFrame)
+dptest = dpc.dataProcess(testFrame)
+
+MeanAge = dp.avgMethod("Age")
+replaceNanInSex = dp.replaceNanInSex("Sex")
+meanFare = dp.avgMethod("Fare")
+replaceNanInEmvarked = dp.replaceNanInEmvarked1("Embarked")
+
+t_MeanAge = dptest.avgMethod("Age")
+t_replaceNanInSex = dptest.replaceNanInSex("Sex")
+t_meanFare = dptest.avgMethod("Fare")
+t_replaceNanInEmvarked = dptest.replaceNanInEmvarked1("Embarked")
+
+#,Fare,Cabin,Embarked
+
+testFrame.PassengerId = testFrame.PassengerId.replace(np.nan,1)
+testFrame.Pclass = testFrame.Pclass.replace(np.nan,3)
+testFrame.Sex = testFrame.Sex.replace(np.nan,replaceNanInSex)
+testFrame.Sex = testFrame.Sex.replace('female',0)
+testFrame.Sex = testFrame.Sex.replace('male',1)
+testFrame.Age = testFrame.Age.replace(np.nan,29)
+testFrame.Fare= testFrame.Fare.replace(np.nan,meanFare)
+#testFrame.Cabin = testFrame.Cabin.replace(np.nan,"unknown")
+testFrame.SibSp = testFrame.SibSp.replace(np.nan,0)
+testFrame.Parch = testFrame.Parch.replace(np.nan,0)
+testFrame.Embarked = testFrame.Embarked.replace(np.nan,replaceNanInEmvarked)
+testFrame.Embarked = testFrame.Embarked.replace('S',1)
+testFrame.Embarked = testFrame.Embarked.replace('C',2)
+testFrame.Embarked = testFrame.Embarked.replace('Q',3)
+
+
+trainFrame.PassengerId = trainFrame.PassengerId.replace(np.nan,1)
+#trainFrame.Survived = trainFrame.Survived.replace(np.nan,1)
+trainFrame.Pclass = trainFrame.Pclass.replace(np.nan,3)
+trainFrame.Sex = trainFrame.Sex.replace(np.nan,replaceNanInSex)
+trainFrame.Sex = trainFrame.Sex.replace('female',0)#z
+trainFrame.Sex = trainFrame.Sex.replace('male',1)
+trainFrame.Age = trainFrame.Age.replace(np.nan,29)
+trainFrame.Fare= trainFrame.Fare.replace(np.nan,meanFare)
+#trainFrame.Cabin = trainFrame.Cabin.replace(np.nan,"unknown")
+trainFrame.SibSp = trainFrame.SibSp.replace(np.nan,0)
+trainFrame.Parch = trainFrame.Parch.replace(np.nan,0)
+trainFrame.Embarked = trainFrame.Embarked.replace(np.nan,replaceNanInEmvarked)
+trainFrame.Embarked = trainFrame.Embarked.replace('S',1)
+trainFrame.Embarked = trainFrame.Embarked.replace('C',2)
+trainFrame.Embarked = trainFrame.Embarked.replace('Q',3)
+
+
+#print(trainFrame.ix[:,6])
+#train data 제거 항목
+
+
+#keras
+print(trainFrame.shape)
+print(testFrame.shape)
+model = Sequential()
+model.add(Dense(units = 100, input_shape =(8,) , activation = 'relu'))
+model.add(Dropout(0.2))
+#model.add(Dense(units = 60, activation = 'relu'))
+#model.add(Dropout(0.6))
+model.add(Dense(units=1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer = 'adam',
+              metrics=['accuracy']
+             )
+model.fit(trainFrame, trainAnswerFrame, epochs=30,  validation_split = 0.2)
+
+model.evaluate(testFrame, testAnswerFrame)
+
+predicted_Y = model.predict(testFrame)
+match = predicted_Y == testAnswerFrame
+wrong_label = np.where(predicted_Y>0.5, 1,0)
+cnt = 0
+
+taf = np.array(testAnswerFile)
+for i in range (wrong_label.shape[0]) :
+    if wrong_label[i] == taf[i] :
+        cnt = cnt + 1
+print("acc : {:d}",  (cnt/wrong_label.shape[0]))
---
+++ DeepLearningProject/Titanic_pandas.py
@@ -0,0 +1,118 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+import tensorflow as tf
+import csv
+import dataProcessClass as dpc
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout 
+from pandas import DataFrame, Series
+
+trainFile = pd.read_csv('C:/Users/Playdata/Desktop/Playdata/PlayData/DeepLearningProject/data/train.csv')
+testFile =  pd.read_csv('C:/Users/Playdata/Desktop/Playdata/PlayData/DeepLearningProject/data/test.csv')
+testAnswerFile =  pd.read_csv('C:/Users/Playdata/Desktop/Playdata/PlayData/DeepLearningProject/data/gender_submission.csv')
+
+trainFrame = DataFrame(trainFile)
+testFrame = DataFrame(testFile)
+testAnswerFrame = DataFrame(testAnswerFile)
+
+trainAnswerFrame = trainFrame['Survived']
+
+del trainFrame['PassengerId']
+del trainFrame['Survived']
+del trainFrame['Ticket']
+del trainFrame['Cabin']
+#test data 제거 항목
+del testFrame['PassengerId']
+del testFrame['Ticket']
+del testFrame['Cabin']
+#testAnswer data 제거 항목
+del testAnswerFrame["Survived"]
+
+
+trainFrame = trainFrame.replace(np.nan,0)
+
+sex = lambda x : 0 if x == 'male' else 1
+age = lambda x : 1 if x <= 1 else 0.9 if x <= 15 else 0.8 if x < 20 else 0.5 if x <= 30 else 0.3 if x <=50 else 0.6 if x <= 60 else 0.8
+pclass = lambda x : 0 if x == 3 else 0.5 if x == 2 else 1
+fare = lambda x : 0 if x < 8 else 0.3 if x <15 else 0.5 if x < 31 else 0.7 if x < 100 else 1
+embarked = lambda x : 0.5 if x =='S'  else 1 if x =='Q'  else 0.7
+name = lambda x : 0 if 'Mr.' in x else 0.5 if 'Dr.' in x else 0.6 if  'Master.' in x  else 0.8 if ('Mrs.' in x )or('Miss' in x )or ('Lady' in x )or('Ms' in x) else 0 
+sibsp = lambda x : x / 8 
+parch = lambda x : x / 6
+print(trainFrame.describe())
+#print(trainFrame.ix[:,0].value_counts())
+# #,Fare,Cabin,Embarked
+
+
+trainFrame.Pclass = trainFrame.Pclass.apply(pclass)
+trainFrame.Sex = trainFrame.Sex.apply(sex)
+trainFrame.Age = trainFrame.Age.apply(age)
+trainFrame.Fare= trainFrame.Fare.apply(fare)
+trainFrame.Embarked = trainFrame.Embarked.apply(embarked) 
+trainFrame.Name = trainFrame.Name.apply(name)
+trainFrame.SibSp = trainFrame.SibSp.apply(sibsp)
+trainFrame.Parch = trainFrame.Parch.apply(parch)
+testFrame.Pclass = testFrame.Pclass.apply(pclass)
+
+testFrame.Sex = testFrame.Sex.apply(sex)
+testFrame.Age = testFrame.Age.apply(age)
+testFrame.Fare= testFrame.Fare.apply(fare)
+testFrame.Embarked = testFrame.Embarked.apply(embarked)
+testFrame.Name = testFrame.Name.apply(name)
+testFrame.SibSp = testFrame.SibSp.apply(sibsp)
+testFrame.Parch = testFrame.Parch.apply(parch)
+
+print(trainFrame)
+# #print(trainFrame.ix[:,6])
+# #train data 제거 항목
+
+#keras
+# print(trainFrame.shape)
+# print(testFrame.shape)
+model = Sequential()
+
+model.add(Dense(units = 200, input_shape =(8,) , activation = 'relu'))
+model.add(Dropout(0.2))
+model.add(Dense(units = 100, input_shape =(8,) , activation = 'relu'))
+model.add(Dropout(0.2))
+model.add(Dense(units = 100, input_shape =(8,) , activation = 'relu'))
+model.add(Dropout(0.2))
+model.add(Dense(units = 50, input_shape =(8,) , activation = 'relu'))
+model.add(Dropout(0.2))
+model.add(Dense(units=1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+               optimizer = 'adam',
+               metrics=['accuracy']
+              )
+model.fit(trainFrame, trainAnswerFrame, epochs = 100 ,  validation_split = 0.2)
+
+model.evaluate(testFrame, testAnswerFrame)
+model.summary()
+predicted_Y = model.predict(testFrame.as_matrix())
+
+match = predicted_Y == testAnswerFrame
+
+wrong_label = np.where(predicted_Y>0.5, 1,0)
+
+testAnswerFrame["Survived"] = wrong_label
+
+#print(testAnswerFrame)
+# del testAnswerFrame[""]
+testAnswerFrame.to_csv("C:/Users/Playdata/Desktop/gender_submission.csv",index = False)
+
+# wrong_label.colums[0] : "PassengerId"
+# wrong_label.colums[1] : "Survived"
+print(pd.read_csv("C:/Users/Playdata/Desktop/gender_submission.csv"))
+
+
+
+# cnt = 0
+
+# taf = np.array(testAnswerFile)
+# for i in range (wrong_label.shape[0]) :
+#     if wrong_label[i] == taf[i] :
+#         cnt = cnt + 1
+# print("acc : {:d}",  (cnt/wrong_label.shape[0]))    
